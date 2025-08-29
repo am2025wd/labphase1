9206f3598c424a2894123717c5e3bb8493df4203
Am@ <halwartech@hotmail.com>
2025-08-29 19:22:47 +0000

    mise a jour server.js

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.use("/api/tasks", require("./routes/taskRoutes"));
 
 // Servir les fichiers statiques en production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "public")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
   });
 }
 
